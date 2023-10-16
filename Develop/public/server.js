a3c34fceff1e070dca52a615eaf7db2e8036bd9c
G303K <micder18@gmail.com>
2023-10-16 17:11:43 -0400

    Added node modules and linked htmt to server.js

---
+++ Develop/public/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
