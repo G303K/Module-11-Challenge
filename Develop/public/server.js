dc989db4652dae3d8817a900fcec5b7a8fbc4b77
G303K <micder18@gmail.com>
2023-10-16 17:32:39 -0400

    Made changes to server.js to display notes

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'index.html'));
 });
 
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname,'notes.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
