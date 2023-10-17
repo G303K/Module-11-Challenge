6cd1da4a415f0ebd127589926ba2c80da797e746
G303K <micder18@gmail.com>
2023-10-17 18:31:33 -0400

    Fixed pathing issue with server.js

--- Develop/server.js
+++ Develop/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'index.html'));
